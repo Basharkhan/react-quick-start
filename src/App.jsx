df3710231e4e56694d7cd0597d380e4f8bf6bd5e
BasharKhan <basharkhan71@gmail.com>
2025-03-16 01:54:53 +0600

    adding logic

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
 
@@ -19,6 +19,8 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+
+  console.log(calculateWinner(squares));
 
   return (
     <>
@@ -49,4 +51,27 @@ function Square({ value, onSquareClick }) {
       </button>
     </>
   );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    console.log(a);
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
